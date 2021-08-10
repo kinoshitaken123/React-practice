3877353803c4431b6e8f0f26c55f090a9cd4ee7b
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-10 23:21:40 +0900

    [プリミティブ型]

---
+++ ts-basic/basic/index.ts
@@ -0,0 +1,11 @@
+// export { default as primitiveSample } from './primitive'{
+//     console.log("Foo")
+// };
+
+export default function primitiveSample() {
+    let name =　'トラハック'
+    console.log("primitive sample 1", typeof name,name)
+
+    let age: number =28
+    console.log("primitive sample 2:", typeof age,age)
+}
--- ts-basic/src/index.ts
+++ ts-basic/src/index.ts
@@ -1,5 +1,11 @@
-import World from './world';
+// import World from './world';
 
-const root = document.getElementById('root')
-const world = new World('Hellow Torahack!!!')
-world.sayHellow(root)
+// const root = document.getElementById('root')
+// const world = new World('Hellow Torahack!!!')
+// world.sayHellow(root)
+// 03. 基本の型定義
+
+
+import { primitiveSample} from './basic'
+
+primitiveSample()
