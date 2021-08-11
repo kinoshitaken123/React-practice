bb4730ee2bf97c45370433d0bf3e01dd3f630e86
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-11 10:10:38 +0900

    [始まりの型　プリミティブ型]

--- ts-basic/basic/index.ts
+++ ts-basic/basic/index.ts
@@ -8,4 +8,10 @@ export default function primitiveSample() {
 
     let age: number =28
     console.log("primitive sample 2:", typeof age,age)
+
+    let isSingle: boolean = true
+    console.log("primitive sample 3:", typeof isSingle, isSingle)
+
+    const isOver20: boolean = age >= 20
+    console.log("primitive sample 4:", typeof isOver20, isOver20)
 }
