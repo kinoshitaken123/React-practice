55feda66930c7aa6f96157eb85cf199140eb5cc5
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-11 15:36:09 +0900

    [レストパラメーターの使い方]

--- ts-basic/src/function/parameters.ts
+++ ts-basic/src/function/parameters.ts
@@ -18,4 +18,9 @@ export const isUserSignedIn2 = (userId: string, username: string = "NO NAME"): b
         return false
     }
    }
-   // オプシ
+// レストパラメーターを持つ関数の例
+export const sumProductsPrice = (...productsPrice: number[]): number => {
+    return productsPrice.reduce((prevTotal, productPrice) => {
+      return prevTotal + productPrice
+    }, 0)
+  }
