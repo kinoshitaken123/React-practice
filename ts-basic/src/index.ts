612eaf6e22a0a7e72df35a7ac390bdc4b0fbef36
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-11 15:22:45 +0900

    [オプションパラメーター　デフォルトパラメーター]

--- ts-basic/src/index.ts
+++ ts-basic/src/index.ts
@@ -5,10 +5,20 @@
 // world.sayHellow(root)
 
 
-// 03.基本の型定義
-import { any, notExist, primitiveSample, unknownSample} from './basic'
+// // 03.基本の型定義
+// import { any, notExist, primitiveSample, unknownSample} from './basic'
 
-any()
-notExist()
-primitiveSample()
-unknownSample()
+// any()
+// notExist()
+// primitiveSample()
+// unknownSample()
+
+// 04.関数の型定義
+
+import {logMessage} from './function/basic'
+import {isUserSignedIn, isUserSignedIn2} from './function/parameters';
+
+logMessage("Hello TypeScript!")
+isUserSignedIn('ABC', 'Torahack')
+isUserSignedIn('123')
+isUserSignedIn2('ABC')
