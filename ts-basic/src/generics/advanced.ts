77754fecceb991982939a441231499af5800006a
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-12 12:03:16 +0900

    [二つ以上のジェネリックス型　パラメーター]

---
+++ ts-basic/src/generics/advanced.ts
@@ -0,0 +1,25 @@
+export default function advancedSample() {
+    // map関数のシグネチャ
+    type Map<T,U> = (arry: T[],fn: (item: T) => U) => U[]
+
+    const mapStringsToNumbers: Map<string, number> = (array, fn) => {
+     const result =[]
+     for (let i =0; i<array.length; i++) {
+       result[i] = fn(array[i])
+     }
+     return result
+    }
+    const numbers = mapStringsToNumbers(['123','456','789'],(item) => Number(item))
+    console.log('Generics advanced sample 1:',numbers)
+  
+    const mapNumbersToStrings: Map<number, string> = (array, fn) => {
+      const result = []
+      for (let i = 0; i < array.length; i++) {
+        result[i] = fn(array[i])
+      }
+      return result
+    }
+  
+    const strings = mapNumbersToStrings(numbers, (item) => String(item))
+    console.log('Generics advanced sample 2:', strings)
+}
