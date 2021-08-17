5b8e4002e6014e4090ea068fd5ceb94fc6f35419
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-17 15:39:22 +0900

    [引数を使ってテキスト入力フォームを作る]

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import Article from "./components/Article";
+import {Article, TextInput} from "./components";
 
 function App() {
   return(
    <div>
-    <Article
-     title={'新日本一わかりやすいReact'}
-     content={'今日のトピックはpropsです'}
-    /> 
-    <Article
+    <TextInput/> 
+    {/* <Article
      title={'新日本一わかりやすいReact2'}
      content={'今日のトピックはpropsです'}
     /> 
     <Article
      title={'新日本一わかりやすいReact3'}
      content={'今日のトピックはpropsです'}
-    /> 
+    />  */}
    </div> 
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Article} from './Article'
 export {default as content} from './Content'
 export {default as title} from './Title'
+export {default as TextInput} from './TextInput'
