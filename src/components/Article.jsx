1abf496d68f067d9edc2e7e01c3cd54e9072064f
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-17 13:56:31 +0900

    [stateをpropsに渡す]

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,23 +2,25 @@
 // ・１ファイルから複数モジュールを読み込む
 // ・エントリポイントから複数コンポーネントを読み込む
 import { useState } from "react";
-import {Content, Title} from "./index";
+import {Content, Title, PublishButton} from "./index";
 
 // propsでデータを受け渡し
 // ・子コンポーネントの引数にpropsを指定する
 // ・親から子にデータを渡す
 
 const Article = (props) => {
-  const [isPublished, setISPublished] = useState(false)
-  console.log(isPublished)
+  const [isPublished, setIsPublished] = useState(false)
+  const publishArticle =() => {
+    setIsPublished(true)
+  }
+
   return (
     <div>
       <Title title={props.title} />
       <Content content={props.content} />
-      <button onClick= {() => setISPublished(true)}>公開</button>
+      <Publishbutton isPublished= {isPublished} onClick ={publishArticle} />
     </div>
   );
 };
 
 export default Article;
-
